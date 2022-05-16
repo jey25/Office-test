4bb3320ea496f8d41481cddbdce3f50383f3648b
jey25 <jjangeoyoung@gmail.com>
2022-05-16 13:32:21 +0900

    2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import express from "express";
 
 const PORT = 4000;
-
 const app = express();
+
+const handlehome = (req, res) => {
+    return res.end()
+}
+
+const handleLogin = (req, res) => {
+    return res.send("Login Here")
+}
+
+app.get("/", handlehome);
+app.get("/login", handleLogin)
 
 const handleListening = () => console.log(`Server ${PORT}`);
 
